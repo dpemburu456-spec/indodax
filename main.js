5df9ff0552a9c1f7985606c91be1d59f8984e779
dpemburu456-spec <dpemburu456@gmail.com>
2026-06-08 19:57:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,9 @@
-const daftarKoin = ['btc_idr', 'eth_idr', 'doge_idr', 'sol_idr', 'xrp_idr', 'ltc_idr', 'arb_idr'];
+import { getCryptoData } from './api.js';
+import { renderCrypto } from './ui.js';
 
-async function updateUI() {
-    const listDiv = document.getElementById('crypto-list');
-    let html = '';
-
-    for (const pair of daftarKoin) {
-        try {
-            // Menggunakan teknik tanpa proxy namun dengan header yang lebih sopan
-            const response = await fetch(`https://indodax.com/api/ticker/${pair}`, {
-                method: 'GET',
-                mode: 'cors',
-                headers: { 'Accept': 'application/json' }
-            });
-            const json = await response.json();
-            const harga = parseInt(json.ticker.last);
-            const nama = pair.split('_')[0].toUpperCase();
-            
-            html += `<div style="padding: 10px; border-bottom: 1px solid #333; color: white; display: flex; justify-content: space-between;">
-                        <span>${nama}</span> <span>Rp ${harga.toLocaleString('id-ID')}</span>
-                     </div>`;
-        } catch (e) {
-            html += `<div style="padding: 10px; color: red;">Akses Ditolak: ${pair}</div>`;
-        }
-    }
-    listDiv.innerHTML = html;
+async function init() {
+    const data = await getCryptoData();
+    renderCrypto(data);
 }
 
-updateUI();
-setInterval(updateUI, 15000);
+init();
