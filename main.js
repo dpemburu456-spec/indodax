59f598f495ad958927cf1eacf1e73ef9174a93cd
dpemburu456-spec <dpemburu456@gmail.com>
2026-06-08 20:09:41 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import { getCryptoData } from './api.js';
-import { renderCrypto } from './ui.js';
-
-async function init() {
-    const data = await getCryptoData();
-    renderCrypto(data);
-}
-
-init();
