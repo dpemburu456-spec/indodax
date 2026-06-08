1cebe53cacdf7aa5547f3ad4e803015983020200
dpemburu456-spec <dpemburu456@gmail.com>
2026-06-08 17:48:20 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,13 +1,10 @@
-// ==========================================
-// FUNGSI MENGAMBIL DATA API INDODAX
-// ==========================================
 async function getIndodaxTicker(pair) {
     try {
-        // MENGGUNAKAN TIMESTAMP AGAR TIDAK MENGAMBIL DATA LAMA (CACHE)
-        const response = await fetch(`https://indodax.com/api/ticker/${pair}?_=${Date.now()}`);
+        const response = await fetch(`https://indodax.com/api/ticker/${pair}?t=${Date.now()}`);
         const data = await response.json();
         return data.ticker;
     } catch (error) {
         return null;
     }
 }
+
