c140429eaf7f34c67e2d91e23933947996cb7570
dpemburu456-spec <dpemburu456@gmail.com>
2026-06-08 21:58:48 +0700

    Delete api.js

--- api.js
+++
@@ -1,5 +0,0 @@
-window.HELAYO_API = (function() {
-    const COINGECKO_BASE = "https://api.coingecko.com/api/v3";
-    // (Isi dengan fungsi fetchTopCoins, fetchFearGreed, dll dari kode asli Anda)
-    return { ... };
-})();
