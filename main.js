665927ae624c99f020880eba84be102e3dc4fb83
dpemburu456-spec <dpemburu456@gmail.com>
2026-06-08 16:51:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 // main.js
+// Daftar koin yang ingin dipantau
+const daftarKoin = ['btc_idr', 'eth_idr', 'doge_idr', 'sol_idr', 'xrp_idr', 'ltc_idr', 'arb_idr'];
+
 async function updateUI() {
     const listDiv = document.getElementById('crypto-list');
-    
-    // Kita panggil fungsi dari api.js
-    const btcData = await getIndodaxTicker('btc_idr');
-    
-    if (btcData) {
-        listDiv.innerHTML = `
-            <div class="row">
-                <span>Bitcoin (BTC)</span>
-                <span style="color: #02c076; font-weight: bold;">
-                    Rp ${parseInt(btcData.last).toLocaleString('id-ID')}
-                </span>
-            </div>
-        `;
-    } else {
-        listDiv.innerHTML = "<p style='color: #cf304a;'>Gagal memuat data.</p>";
+    let htmlContent = '';
+
+    for (const pair of daftarKoin) {
+        const data = await getIndodaxTicker(pair); // Memanggil api.js
+        if (data) {
+            const namaKoin = pair.split('_')[0].toUpperCase();
+            htmlContent += `
+                <div class="row" style="display: flex; justify-content: space-between; margin-bottom: 10px; padding: 10px; border-bottom: 1px solid #333;">
+                    <span style="color: #fff; font-weight: bold;">${namaKoin}</span>
+                    <span style="color: #02c076; font-weight: bold;">
+                        Rp ${parseInt(data.last).toLocaleString('id-ID')}
+                    </span>
+                </div>
+            `;
+        }
     }
+    listDiv.innerHTML = htmlContent;
 }
 
-// Jalankan update setiap 5 detik
+// Update data setiap 5 detik
 updateUI();
 setInterval(updateUI, 5000);
-
